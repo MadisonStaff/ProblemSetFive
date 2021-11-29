280c8aea78852c8a1c56a95262ea97489c488fcd
MadisonStaff <91098954+MadisonStaff@users.noreply.github.com>
2021-11-29 11:45:04 -0500

    Code for our text based game

--- src/TextBasedAdventureGame.java
+++ src/TextBasedAdventureGame.java
@@ -10,29 +10,38 @@
             Random rand = new Random();
 
             // Game variables
-            String[] enemies = {"Hypothermia", "Jaguar", "Poison", "Infection", "Starvation`"};
-            int maxEnemyHealth = 75;
-            int enemyAttackDamage = 25;
+            String[] enemies = {"Hypothermia", "Jaguar", "Poison", "Starvation"};
 
-            //Player Variables
-            int health = 100;
 
         }
     }
-}
-
-        class Enemy {
-            public static void Jaguar() {
-                String choice;
-                Scanner in = new Scanner(System.in);
-                choice = in.next();
-                System.out.println("A wild jaguar has appeared!");
-                System.out.println("What would you like to do?");
-                System.out.println("1. Run");
-                System.out.println("2. Fight");
-                choice = in.next();
-
-
-
+// A return statement of 1 means the player dies a return statement of 2 means the player survives.
+        public static int Jaguar() {
+            int choice;
+            Scanner in = new Scanner(System.in);
+            System.out.println("A wild jaguar has appeared!");
+            System.out.println("What would you like to do?");
+            System.out.println("1. Run");
+            System.out.println("2. Fight");
+            choice = in.nextInt();
+            if (choice == 1) {
+                System.out.println("The jaguar chased you down, and you died.");
+                return 1;
+            } else {
+                System.out.println("You hit the jaguar over the head with a rock, and it ran away!");
+                return 2;
             }
         }
+    }
+
+        class Enemy {
+            public static int Hypothermia() {
+                int choice;
+                Scanner in = new Scanner(System.in);
+                System.out.println("Your fingers are turning blue you have hypothermia. What would you like to do?");
+                System.out.println("1. ignore it");
+                System.out.println("2. build a shelter ");
+
+            }
+    }
+

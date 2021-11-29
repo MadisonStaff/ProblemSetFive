cf68defbe0ad328e65dc25e4648870093e7bf7f9
MadisonStaff <91098954+MadisonStaff@users.noreply.github.com>
2021-11-29 12:10:40 -0500

    Code for our text based game

--- src/TextBasedAdventureGame.java
+++ src/TextBasedAdventureGame.java
@@ -12,6 +12,14 @@
             // Game variables
             String[] enemies = {"Hypothermia", "Jaguar", "Poison", "Starvation"};
 
+            boolean running = true;
+
+            System.out.println("You are stranded on a desert island!");
+
+            GAME:
+            while (running) {
+
+            }
 
         }
     }
@@ -27,10 +35,58 @@
             if (choice == 1) {
                 System.out.println("The jaguar chased you down, and you died.");
                 return 1;
-            } else {
+            } else if (choice == 2) {
                 System.out.println("You hit the jaguar over the head with a rock, and it ran away!");
                 return 2;
             }
+            else {
+                return 3;// invalid return statement
+            }
+        }
+
+        public static int Venom() {
+            int choice;
+            Scanner in = new Scanner(System.in);
+            System.out.println("Tststststs. You have come across a fierce rattlesnake. What should you do?");
+            System.out.println("1. Pick up the sword to your left and fight it");
+            System.out.println("2. Run away");
+            choice = in.nextInt();
+            if (choice == 1) {
+                System.out.println("You are running away, but the snake has caught up. It has bitten you in the leg and" +
+                        "you perish from its venom");
+                return 1;
+            } else {
+                System.out.println("You grab the sword. In one fowl swoop, you slay the snake and are unscathed");
+                return 2;
+            }
+        }
+
+        public static int Starvation() {
+            int choice;
+            Scanner in = new Scanner(System.in);
+            System.out.println("You haven't eaten in two days and need to find food. What would you like to do?");
+            System.out.println("1. Make a spear and kill wild boar.");
+            System.out.println("2. Eat wild berries in the jungle.");
+            System.out.println("3. Make a fishing pole to catch tuna. ");
+            System.out.println("4. Do nothing.");
+
+            choice = in.nextInt();
+            if (choice == 1) {
+                System.out.println("You regain enough energy to improve shelter.");
+                return 2;
+            } else if (choice == 2) {
+                System.out.println("The berries were poisonous and you died.");
+                return 1;
+            } else if (choice == 3) {
+                System.out.println("You have a little more energy but are still need more food.");
+                return 2;
+            } else if (choice == 4) {
+                System.out.println("You died from starvation.");
+                return 1;
+            } else {
+                return 3;
+            }
+
         }
     }
 
@@ -41,7 +97,25 @@
                 System.out.println("Your fingers are turning blue you have hypothermia. What would you like to do?");
                 System.out.println("1. ignore it");
                 System.out.println("2. build a shelter ");
-
+                System.out.println("3. insulate the ground with leaves");
+                choice = in.nextInt();
+                if (choice == 1) {
+                    System.out.println("You ignored it, and you died.");
+                    return 1;
+                }
+                else if (choice == 2) {
+                    System.out.println("You built a shelter to stay warm!");
+                    return 2;
+                }
+                else if (choice == 3) {
+                    System.out.println("You insulated the ground with leaves to stay warm!");
+                    return 2;
+                }
+                else {
+                    return 3;
+                }
             }
-    }
+        }
+
+
 

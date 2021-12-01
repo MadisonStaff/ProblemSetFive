8aa060d1506dfc13d60bc1274fc5906e075d4836
Henry Nolte <nolthr18@wfu.edu>
2021-12-01 11:52:36 -0500

    while loop update

--- src/TextBasedAdventureGame.java
+++ src/TextBasedAdventureGame.java
@@ -1,21 +1,23 @@
-    import java.util.Random;
-    import java.util.Scanner;
+import java.util.Random;
+import java.util.Scanner;
 
-    public class TextBasedAdventureGame {
-        public static void main(String[] args) {
+public class TextBasedAdventureGame {
+    public static void main(String[] args) {
 
-            //System objects
-            Scanner in = new Scanner(System.in);
-            Random rand = new Random();
-            int j = 0;
-            int score = 0;
-            boolean running = true;
+        //System objects
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        int j = 0;
+        int score = 0;
+        boolean running = true;
+        int input;
 
 
-            // Game variables
-            String[] enemies = {"Hypothermia", "Jaguar", "Venom", "Starvation"};
-            System.out.println("You are stranded in the jungle!");
+        // Game variables
+        String[] enemies = {"Hypothermia", "Jaguar", "Venom", "Starvation"};
+        System.out.println("You are stranded in the jungle!");
 
+        while (running) {
 
             // GAME:
             while (running) {
@@ -57,149 +59,162 @@
 
             }
 
-        //call the method Starvation here
+            //call the method Starvation here
             while (running) {
-            int choice = Starvation();
-            if (choice == 1) {
-                System.out.println("-----DEATH------");
-                System.out.println("You have lost the possible point for this round.");
-                break;
+                int choice = Starvation();
+                if (choice == 1) {
+                    System.out.println("-----DEATH------");
+                    System.out.println("You have lost the possible point for this round.");
+                    break;
+                }
+                if (choice == 2) {
+                    System.out.println("You survived to the next obstacle!");
+                    score++;
+                    break;
+                }
+                else if (Starvation() == 3) {
+                }
             }
-            if (choice == 2) {
-                System.out.println("You survived to the next obstacle!");
-                score++;
-                break;
-            }
-            else if (Starvation() == 3) {
-            }
-        }
-            while (running)
-        {
-            int choice = Hypothermia();
+            while (running) {
+                int choice = Hypothermia();
 
             /*
             call method hypothermia here
              */
 
-            if (choice == 1) {
-                System.out.println("-----DEATH------");
-                System.out.println("You have lost the possible point for this round.");
-                break;
+                if (choice == 1) {
+                    System.out.println("-----DEATH------");
+                    System.out.println("You have lost the possible point for this round.");
+                    break;
+                }
+                if (choice == 2) {
+                    System.out.println("You survived to the next obstacle!");
+                    score++;
+                }
+                if (Hypothermia() == 3) {
+                }
+
+                System.out.println("------CONGRATULATIONS-------");
+                System.out.println("You have made it to the end of the Jungle challenge!");
+                System.out.println("your score is " + ((score / 4) * 100) + "%");
+
             }
-            if (choice == 2) {
-                System.out.println("You survived to the next obstacle!");
-                score++;
+
+            System.out.println("Enter 0 if you want to continue or 1 to stop");
+            input = in.nextInt();
+
+            if(input == 0) {
+                System.out.println("You continue on your adventure!");
             }
-            if (Hypothermia() == 3) {
+            else if(input == 1) {
+                System.out.println("Thanks for playing!");
+                running = false;
             }
         }
-            System.out.println("------CONGRATULATIONS-------");
-            System.out.println("You have made it to the end of the Jungle challenge!");
-            System.out.println("your score is "+((score /4)*100)+"%");
 
     }
 
 
 
 
-/**
- * A return statement of 1 means the player dies a return statement of 2 means the player survives.
-*/
-        public static int Jaguar() {
-            int choice;
-            Scanner in = new Scanner(System.in);
-            System.out.println("A wild jaguar has appeared!");
-            System.out.println("What would you like to do?");
-            System.out.println("1. Run");
-            System.out.println("2. Fight");
-            choice = in.nextInt();
-            if (choice == 1) {
-                System.out.println("The jaguar chased you down, and you died.");
-                return 1;
-            } else if (choice == 2) {
-                System.out.println("You hit the jaguar over the head with a rock, and it ran away!");
-                return 2;
-            }
-            else {
-                return 3;// invalid return statement
-            }
+    /**
+     * A return statement of 1 means the player dies a return statement of 2 means the player survives.
+     */
+    public static int Jaguar() {
+        int choice;
+        Scanner in = new Scanner(System.in);
+        System.out.println("A wild jaguar has appeared!");
+        System.out.println("What would you like to do?");
+        System.out.println("1. Run");
+        System.out.println("2. Fight");
+        choice = in.nextInt();
+        if (choice == 1) {
+            System.out.println("The jaguar chased you down, and you died.");
+            return 1;
+        } else if (choice == 2) {
+            System.out.println("You hit the jaguar over the head with a rock, and it ran away!");
+            return 2;
+        }
+        else {
+            return 3;// invalid return statement
+        }
+    }
+
+    public static int Venom() {
+        int choice;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Tststststs. You have come across a fierce rattlesnake. What should you do?");
+        System.out.println("1. Pick up the sword to your left and fight it");
+        System.out.println("2. Run away");
+        choice = in.nextInt();
+        if (choice == 2) {
+            System.out.println("You are running away, but the snake has caught up. It has bitten you in the leg and" +
+                    "you perish from its venom");
+            return 1;
+        } if (choice == 1){
+            System.out.println("You grab the sword. In one fowl swoop, you slay the snake and are unscathed");
+            return 2;
+        }
+        else {
+            return 3;
+        }
+    }
+
+    public static int Starvation() {
+        int choice;
+        Scanner in = new Scanner(System.in);
+        System.out.println("You haven't eaten in two days and need to find food. What would you like to do?");
+        System.out.println("1. Make a spear and kill wild boar.");
+        System.out.println("2. Eat wild berries in the jungle.");
+        System.out.println("3. Make a fishing pole to catch tuna. ");
+        System.out.println("4. Do nothing.");
+
+        choice = in.nextInt();
+        if (choice == 1) {
+            System.out.println("You regain enough energy to improve shelter.");
+            return 2;
+        } else if (choice == 2) {
+            System.out.println("The berries were poisonous and you died.");
+            return 1;
+        } else if (choice == 3) {
+            System.out.println("You have a little more energy but are still need more food.");
+            return 2;
+        } else if (choice == 4) {
+            System.out.println("You died from starvation.");
+            return 1;
+        } else {
+            return 3;
         }
 
-        public static int Venom() {
-            int choice;
-            Scanner in = new Scanner(System.in);
-            System.out.println("Tststststs. You have come across a fierce rattlesnake. What should you do?");
-            System.out.println("1. Pick up the sword to your left and fight it");
-            System.out.println("2. Run away");
-            choice = in.nextInt();
-            if (choice == 2) {
-                System.out.println("You are running away, but the snake has caught up. It has bitten you in the leg and" +
-                        "you perish from its venom");
-                return 1;
-            } if (choice == 1){
-                System.out.println("You grab the sword. In one fowl swoop, you slay the snake and are unscathed");
-                return 2;
-            }
-            else {
-                return 3;
-            }
+    }
+
+
+
+    public static int Hypothermia() {
+        int choice;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Your fingers are turning blue you have hypothermia. What would you like to do?");
+        System.out.println("1. ignore it");
+        System.out.println("2. build a shelter ");
+        System.out.println("3. insulate the ground with leaves");
+        choice = in.nextInt();
+        if (choice == 1) {
+            System.out.println("You ignored it, and you died.");
+            return 1;
         }
-
-        public static int Starvation() {
-            int choice;
-            Scanner in = new Scanner(System.in);
-            System.out.println("You haven't eaten in two days and need to find food. What would you like to do?");
-            System.out.println("1. Make a spear and kill wild boar.");
-            System.out.println("2. Eat wild berries in the jungle.");
-            System.out.println("3. Make a fishing pole to catch tuna. ");
-            System.out.println("4. Do nothing.");
-
-            choice = in.nextInt();
-            if (choice == 1) {
-                System.out.println("You regain enough energy to improve shelter.");
-                return 2;
-            } else if (choice == 2) {
-                System.out.println("The berries were poisonous and you died.");
-                return 1;
-            } else if (choice == 3) {
-                System.out.println("You have a little more energy but are still need more food.");
-                return 2;
-            } else if (choice == 4) {
-                System.out.println("You died from starvation.");
-                return 1;
-            } else {
-                return 3;
-            }
-
+        else if (choice == 2) {
+            System.out.println("You built a shelter to stay warm!");
+            return 2;
         }
-
-
-
-            public static int Hypothermia() {
-                int choice;
-                Scanner in = new Scanner(System.in);
-                System.out.println("Your fingers are turning blue you have hypothermia. What would you like to do?");
-                System.out.println("1. ignore it");
-                System.out.println("2. build a shelter ");
-                System.out.println("3. insulate the ground with leaves");
-                choice = in.nextInt();
-                if (choice == 1) {
-                    System.out.println("You ignored it, and you died.");
-                    return 1;
-                }
-                else if (choice == 2) {
-                    System.out.println("You built a shelter to stay warm!");
-                    return 2;
-                }
-                else if (choice == 3) {
-                    System.out.println("You insulated the ground with leaves to stay warm!");
-                    return 2;
-                }
-                else {
-                    return 3;
-                }
-            }
+        else if (choice == 3) {
+            System.out.println("You insulated the ground with leaves to stay warm!");
+            return 2;
         }
+        else {
+            return 3;
+        }
+    }
+}
 
 
 
